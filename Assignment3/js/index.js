cbe42081877172b36362d4d0d7701b28f19d0e4e
NhatPham <phamnhat05111997@gmail.com>
2023-01-03 20:35:44 +0700

    add ẩn thông tin

--- Assignment3/js/index.js
+++ Assignment3/js/index.js
@@ -1,1 +1,20 @@
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+const checkRegex = new RegExp(regex);
+function submit() {
+    let email = document.getElementById('validationEmails').value
+    if(email === '') {
+        document.getElementById('invalid-feedback-entry').classList.remove('d-none')
+        document.getElementById('invalid-feedback-entry').classList.add('d-block')
+    } else {
+        if(checkRegex.test(email)) {
+            document.getElementById('form-info').classList.add('d-none')
+            document.getElementById('invalid-feedback-error').classList.add('d-none')
+            document.getElementById('personal-info-content').classList.remove('d-none')
+        }else {
+            document.getElementById('invalid-feedback-entry').classList.add('d-none')
+            document.getElementById('invalid-feedback-error').classList.remove('d-none')
+            document.getElementById('invalid-feedback-error').classList.add('d-block')
+        }
+    }
+}
