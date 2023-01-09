2fb7b387075a59e930eeb4a2d6a3c5d8f9ecd712
NhatPham <phamnhat05111997@gmail.com>
2023-01-09 19:58:03 +0700

    update

--- Lab20.1/index.js
+++ Lab20.1/index.js
@@ -3,11 +3,7 @@ function showModal() {
     document.querySelector('.overlay').classList.remove('hidden');
     document.querySelector('.modal').classList.remove('hidden');
 }
-document.querySelector('.close-modal').addEventListener('click', function() {
+function closeModal() {
     document.querySelector('.overlay').classList.add('hidden');
     document.querySelector('.modal').classList.add('hidden');
-})
-document.querySelector('.overlay').addEventListener('click', function() {
-    document.querySelector('.overlay').classList.add('hidden');
-    document.querySelector('.modal').classList.add('hidden');
-})
+}
