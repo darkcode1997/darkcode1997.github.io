80928b5842a3add0e4ef228e55ded255517f12c0
darkcode <darkcode>
2023-01-09 17:31:10 +0700

    update

--- Lab19/index.js
+++ Lab19/index.js
@@ -1,2 +1,36 @@
 'use strict'
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!'
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent = 20
+// document.querySelector('.guess').value = 23
+const number = Math.trunc(Math.random()*20) + 1;
+let scoreNumber = 20
+document.querySelector('.check').addEventListener('click', () => {
+    const guess =  document.querySelector('.guess').value 
+    if(!guess) {
+        document.querySelector('.message').textContent = 'No Number !'
+    }else if(number === Number(guess)) {
+        document.querySelector('.number').textContent = number
+        document.querySelector('.message').textContent = 'Correct Number!'
+        document.querySelector('body').style.backgroundColor = '#60b347';
+    } else if(number > guess) {
+        if(scoreNumber > 0) {
+            document.querySelector('.message').textContent = 'To Low!'
+            scoreNumber--
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game'
+        }
+        document.querySelector('.score').textContent = scoreNumber
+    }else if(number < guess) {
+        scoreNumber--
+        document.querySelector('.message').textContent = 'To High'
+        document.querySelector('.score').textContent = scoreNumber
+    }
+})
+document.querySelector('.again').addEventListener('click', () => {
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.score').textContent = 20
+    document.querySelector('.guess').value = ''
+    document.querySelector('body').style.backgroundColor = '#222';
+})
